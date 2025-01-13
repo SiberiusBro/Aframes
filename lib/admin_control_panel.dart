9acb0f553082306866567f2481b2a28e5dd5f04c
Ene Sebastian <eneionsebastyan@gmail.com>
2025-01-13 19:18:39 +0200

    App initial

--- lib/admin_control_panel.dart
+++ lib/admin_control_panel.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'pages/welcome_page.dart';
 import 'back button/CustomAppBar.dart';
 
 class AdminControlPanel extends StatelessWidget {
   const AdminControlPanel({super.key});
+
+  // Function to open GPS location
+  Future<void> _openMap() async {
+    const latitude = 45.09220837714519; // Example latitude
+    const longitude = 24.366372986649264; // Example longitude
+    final googleMapsUrl =
+        'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
+    if (await canLaunch(googleMapsUrl)) {
+      await launch(googleMapsUrl);
+    } else {
+      throw 'Could not open the map.';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +37,36 @@ class AdminControlPanel extends StatelessWidget {
             colors: [Colors.green, Colors.lightGreen],
           ),
         ),
-        child: const Center(
-          child: Text(
-            'Welcome Admin',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Welcome Admin',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: _openMap,
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 12.0,
+                    horizontal: 24.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Go to Location',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
           ),
         ),
       ),
